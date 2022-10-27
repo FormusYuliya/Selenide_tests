5adf194214c1579869f08c62d076dc52791631dd
FormusYuliya <75179001+FormusYuliya@users.noreply.github.com>
2022-10-27 21:42:07 +0300

    JUnit + working Allure

--- src/main/java/pages/SignInPage.java
+++ src/main/java/pages/SignInPage.java
@@ -1,6 +1,7 @@
 package pages;
 
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.*;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
--- src/test/java/CreateContent.java
+++ src/test/java/CreateContent.java
@@ -1,4 +1,8 @@
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
 import org.junit.Test;
+
 import pages.*;
 
 import java.io.File;
@@ -74,7 +78,10 @@ public class CreateContent extends BaseTest implements SelectCouponTheme, Select
     }
 
     @Test
+    @Severity(SeverityLevel.BLOCKER)
     public void createFact() {
+
+
         SignInPage signInPage = new SignInPage();
         signInPage.openPage();
         ProjectDashboardPage projectDashboardPage = signInPage.loginAs("admin@sodyo.com", "So123456");
